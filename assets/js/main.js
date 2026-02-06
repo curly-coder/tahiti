f92e3d83b851750c36e292f6ab0c204a4ab55480
curly-coder <brat.thor8@gmail.com>
2026-02-06 11:04:12 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,         
+    // autoplaySpeed: 800,   
+    arrows: false,          
+    dots: false,            
+    fade: true,   
+  });
+});
+
+$(document).ready(function(){
+  function initSlider() {
+    if ($(window).width() < 1024) {
+      if (!$('.islands-slider').hasClass('slick-initialized')) {
+        $('.islands-slider').slick({
+          slidesToShow: 2,  
+          slidesToScroll: 1, 
+          arrows: false,   
+          dots: false,     
+          infinite: false, 
+          swipeToSlide: true,
+          responsive: [
+            {
+              breakpoint: 490,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+      }
+    } else {
+      if ($('.islands-slider').hasClass('slick-initialized')) {
+        $('.islands-slider').slick('unslick');
+      }
+    }
+  }
+
+  initSlider();
+  $(window).resize(function(){
+    initSlider();
+  });
+});
+
+document.querySelector('.explore-btn').addEventListener('click', function() {
+  const select = document.querySelector('.island-select');
+  const value = select.value;
+  if (value) {
+    alert("You selected: " + value);
+  }
+});
+
